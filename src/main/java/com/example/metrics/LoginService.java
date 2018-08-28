0097ab9970f4da1e6cdf39589b14b36c27bc7e9c
shalimalli <42216250+shalimalli@users.noreply.github.com>
2018-08-28 15:39:54 +0530

    test

--- src/main/java/com/example/metrics/LoginService.java
+++ src/main/java/com/example/metrics/LoginService.java
@@ -1,5 +1,8 @@
 package com.example.metrics;
 
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +16,12 @@ public class LoginService {
 	private MeterRegistry meterRegistry;
 	
 	@RequestMapping("/login")
-	public String login() {
+	public String login() throws InterruptedException {
+		long start = Calendar.getInstance().getTimeInMillis();
+		Thread.sleep(2000);
 		meterRegistry.counter("counter.login.success").increment();
+		long end = Calendar.getInstance().getTimeInMillis();
+		meterRegistry.timer("timer.login").record(end-start, TimeUnit.MILLISECONDS);;
 		return "Login success";
 	}
 }
